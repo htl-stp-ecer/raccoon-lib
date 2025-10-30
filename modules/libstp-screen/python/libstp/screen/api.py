28f05ddc15a2942992e6ad63cc1430ce973241be
eternalRose <flo.ninja07@gmail.com>
2025-10-30 13:21:19 +0100

    created an api endpoint for the wait for light feature

--- modules/libstp-screen/python/libstp/screen/api.py
+++ modules/libstp-screen/python/libstp/screen/api.py
@@ -18,10 +18,10 @@ class RenderScreen(ClassNameLogger):
         self.LCM = lcm.LCM()
         self.cancel_event = asyncio.Event()
         self.sensor = AnalogSensor(port)
-        self.LCM.subscribe("libstp/screen_render/cancel", self._handle_cancel_request)
-        asyncio.create_task(self._lcm_pump_async())
+        self.LCM.subscribe("libstp/screen_render/cancel", self.__handle_cancel_request)
+        asyncio.create_task(self.__lcm_pump_async())
 
-    def _handle_cancel_request(self, channel, data):
+    def __handle_cancel_request(self, channel, data):
         self.warn("Something went wrong! Try check the data you send!")
         self.cancel_event.set()
 
@@ -37,7 +37,7 @@ class RenderScreen(ClassNameLogger):
     def send_state(self, data: Dict[str, Any]):
         self.__send_screen_render_request_to_lcm(data)
 
-    async def wait_for_lcm_message(self, timeout: float = 10.0) -> Any:
+    async def __wait_for_lcm_message(self, timeout: float = 10.0) -> Any:
         loop = asyncio.get_event_loop()
         future = loop.create_future()
 
@@ -57,17 +57,17 @@ class RenderScreen(ClassNameLogger):
         finally:
             self.LCM.unsubscribe(sub)
 
-    async def wait_for_finish(self, timeout: float = 10.0) -> Any:
-        return await self.wait_for_lcm_message(timeout=timeout)
+    async def __wait_for_finish(self, timeout: float = 10.0) -> Any:
+        return await self.__wait_for_lcm_message(timeout=timeout)
 
-    async def wait_for_button(self):
+    async def __wait_for_button(self):
         for _ in range(10):
             if self.cancel_event.is_set():
                 self.debug("Cancelled wait_for_button")
                 raise asyncio.CancelledError()
             await asyncio.sleep(1)
 
-    def getAnalogValue(self):
+    def __get_analog_value(self):
         value = self.sensor.read()
         while value == 0:
             value = self.sensor.read()
@@ -78,21 +78,21 @@ class RenderScreen(ClassNameLogger):
         self.change_screen("calibrate_sensors")
 
         if self.cancel_event.is_set():
-            self.debug("Calibration cancelled before start")
+            self.debug("Black White Sensor calibration cancelled before start")
             return
 
         self.debug("Calibrating black request")
         self.send_state({"port": self.port, "type": "blackWhite", "state": "calibrate_black"})
 
-        await self.wait_for_button()
-        black_value = self.getAnalogValue()
+        await self.__wait_for_button()
+        black_value = self.__get_analog_value()
         self.debug("Black Value: " + str(black_value))
 
         self.debug("Calibrating white request")
         self.send_state({"port": self.port, "type": "blackWhite", "state": "calibrate_white"})
 
-        await self.wait_for_button()
-        white_value = self.getAnalogValue()
+        await self.__wait_for_button()
+        white_value = self.__get_analog_value()
         self.debug("White Value: " + str(white_value))
 
         self.debug("Time to confirm")
@@ -105,7 +105,7 @@ class RenderScreen(ClassNameLogger):
         })
 
         try:
-            msg = await self.wait_for_finish(timeout=120)
+            msg = await self.__wait_for_finish(timeout=120)
         except asyncio.CancelledError:
             self.debug("Calibration cancelled during wait_for_finish")
             return
@@ -122,7 +122,49 @@ class RenderScreen(ClassNameLogger):
 
         print(f"Set values to: black {black_value} white {white_value}")
 
-    async def _lcm_pump_async(self):
+    async def calibrate_wfl(self, trie=0):
+        self.cancel_event.clear()
+        self.change_screen("calibrate_sensors")
+
+        if self.cancel_event.is_set():
+            self.debug("Wait for light calibration cancelled before start")
+            return
+
+        self.debug("Calibrating wfl off request")
+        self.send_state({"port": self.port, "type": "waitForLight", "state": "calibrate_wfl_off"})
+
+        await self.__wait_for_button()
+        wfl_off_value = self.__get_analog_value()
+        self.debug("Wait for light off Value: " + str(wfl_off_value))
+
+        self.debug("Calibrating wfl on request")
+        self.send_state({"port": self.port, "type": "waitForLight", "state": "calibrate_wfl_on"})
+
+        await self.__wait_for_button()
+        wfl_on_value = self.__get_analog_value()
+        self.debug("Wait for light on Value: " + str(wfl_on_value))
+
+        self.send_state({"port": self.port, "type": "waitForLight", "state": "confirm", "wfl_off_value": wfl_off_value, "wfl_on_value": wfl_on_value})
+
+        try:
+            msg = await self.__wait_for_finish(timeout=120)
+        except asyncio.CancelledError:
+            self.debug("Calibration cancelled during wait_for_finish")
+            return
+        except asyncio.TimeoutError:
+            self.debug("Calibration timeout during wait_for_finish")
+            msg = "retry"
+
+        if msg == "retry":
+            trie += 1
+            if trie >= 5:
+                self.warn("Could not finish wait for light calibration")
+                return
+            await self.calibrate_wfl(trie)
+        print(f"Set value to: wfl {wfl_off_value}")
+
+
+    async def __lcm_pump_async(self):
         while True:
             self.LCM.handle_timeout(0)
             await asyncio.sleep(0.01)
