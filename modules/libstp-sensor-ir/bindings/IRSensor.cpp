e02d0f51642abe646da90c0d38f14f6d6a1af3cd
eternalRose <flo.ninja07@gmail.com>
2025-11-25 14:33:23 +0100

    irsensor python version

--- modules/libstp-sensor-ir/bindings/IRSensor.cpp
+++ modules/libstp-sensor-ir/bindings/IRSensor.cpp
@@ -6,10 +6,11 @@
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
-
-void init_analog(const py::module& m) {
+void init_ir_sensor(const py::module& m) {
     py::class_<libstp::sensors::ir::IRSensor>(m, "IRSensor")
         .def(py::init<const int&, float>(),
              py::arg("port"),
-             py::arg("calibrationFactor"));
+             py::arg("calibrationFactor"))
+        .def("setCalibration", &libstp::sensors::ir::IRSensor::setCalibration )
+        .def("read", &libstp::sensors::ir::IRSensor::read);
 }
