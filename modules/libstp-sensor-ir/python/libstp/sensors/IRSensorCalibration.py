31b012ab053f735e3f135f11d4f2dbbed16c51e0
eternalRose <flo.ninja07@gmail.com>
2025-12-04 10:20:14 +0100

    kmeans now

--- modules/libstp-sensor-ir/python/libstp/sensors/IRSensorCalibration.py
+++ modules/libstp-sensor-ir/python/libstp/sensors/IRSensorCalibration.py
@@ -1,5 +1,6 @@
-import math
 import time
+import numpy as np
+from sklearn.cluster import KMeans
 
 from libstp.sensor_ir import IRSensor
 import libstp.button as Button
@@ -9,70 +10,34 @@ class IRSensorCalibration:
         Button.set_digital(buttonPort)
 
     def calibrateSensors(self, irsensors):
-        print("Press the button when ready to scan the white values")
+        print("Press the button when ready to scan the values")
         Button.wait_for_button_press()
-        print("Collecting data")
-        t_end = time.time() + 10
-        whiteValues = []
-        blackValues = []
-        while time.time() < t_end:
-            for irsensor in irsensors:
-                whiteValues.append(irsensor.read())
-                time.sleep(0.01)
+        values = self._collectValues(irsensors, 10)
 
-        print("Press the button when ready to scan the black values")
-        Button.wait_for_button_press()
-        print("Collecting data")
-        t_end = time.time() + 10
-
-        while time.time() < t_end:
-            for irsensor in irsensors:
-                blackValues.append(irsensor.read())
-                time.sleep(0.01)
-
-        print("Done now calibrating")
-
-        if len(whiteValues) == 0 or len(blackValues) == 0:
-            print("Got no Values for either White or Black")
+        if not values:
+            print("Got no values for WHITE or BLACK")
             return
 
-        for i in irsensors:
-            self.calibrateSingle(i, blackValues, whiteValues)
+        for sensor in irsensors:
+            self.calibrateSingle(sensor, values)
 
+    def _collectValues(self, irsensors, duration=5):
+        print("Collecting Values")
+        t_end = time.time() + duration
+        values = []
+        while time.time() < t_end:
+            for sensor in irsensors:
+                values.append(sensor.read())
+                time.sleep(0.01)
+        return values
 
-    def __mean(self, v):
-        return sum(v) / float(len(v))
-
-    def __stddev(self, v, mean):
-        return math.sqrt(sum((val - mean) ** 2 for val in v) / len(v))
-
-    def calibrateSingle(self, irsensor, blackValues, whiteValues):
-        whiteMean = self.__mean(whiteValues)
-        blackMean = self.__mean(blackValues)
-
-        if whiteMean >= blackMean:
-            print(f"Calibration error: White values (mean: {whiteMean}) should be lower than black values (mean: {blackMean})",)
-            print("This could indicate the sensors are positioned incorrectly or ambient light issues")
-            return False
-
-        whiteStdDev = max(1.0, self.__stddev(whiteValues, whiteMean))
-        blackStdDev = max(1.0, self.__stddev(blackValues, blackMean))
-
-        delta = blackMean - whiteMean
-
-        if delta < 100.0:
-            print(f"Insufficient contrast between white (mean: {whiteMean}) and black (mean: {blackMean}) values")
-            print(f"Delta = {delta}. Recommended minimum delta is 100")
-            return False
-
-        if whiteStdDev > 0.2 * delta or blackStdDev > 0.2 * delta:
-            print(f"High variance in readings: white stddev = {whiteStdDev}, black stddev = {blackStdDev}")
-            print("This might indicate uneven lighting or unstable sensor position")
-
-        #todo maybe remove factor later
-        whiteThresh = (whiteMean + 0.5 * delta)
-        blackThresh = (blackMean - 0.5 * delta)
-        irsensor.setCalibration(whiteThresh, blackThresh)
-        print(f"Calibration successful: white mean = {whiteMean}, black mean = {blackMean}")
-        print(f"Thresholds set: white = {whiteThresh}, black = {blackThresh}")
+    def calibrateSingle(self, irsensor, values):
+        all_values = np.array(values).reshape(-1, 1)
+        kmeans = KMeans(n_clusters=2, n_init=10, random_state=42)
+        kmeans.fit(all_values)
+        centers = sorted(kmeans.cluster_centers_.flatten())
+        white_center = centers[0]
+        black_center = centers[1]
+        irsensor.setCalibration(black_center, white_center)
+        print(f"White center: {white_center}, Black center: {black_center}")
         return True
