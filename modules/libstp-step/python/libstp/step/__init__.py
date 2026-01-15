15fd4b664ec762fa971b9a1aee5010ea2cfcb654
tobias Madlberger <tobias.madlberger@gmail.com>
2026-01-15 09:09:40 +0100

    added imports

--- modules/libstp-step/python/libstp/step/__init__.py
+++ modules/libstp-step/python/libstp/step/__init__.py
@@ -1,12 +1,14 @@
 from .base import Step
 from .model import StepProtocol, SimulationStepDelta, SimulationStep
 from .sequential import Sequential, seq
-
+from .calibrate import calibrate_sensors, CalibrateSensors
 __all__ = [
     "Step",
     "StepProtocol",
     "SimulationStepDelta",
     "SimulationStep",
     "Sequential",
-    "seq"
+    "seq",
+    "calibrate_sensors",
+    "CalibrateSensors",
 ]
