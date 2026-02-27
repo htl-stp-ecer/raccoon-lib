58d2834b21b72b9bc49238de124bcc70a21c1631
eternalRose <flo.ninja07@gmail.com>
2026-02-27 10:11:19 +0100

    fixing things

--- tests/cpp/kinematics/test_differential_kinematics.cpp
+++ tests/cpp/kinematics/test_differential_kinematics.cpp
@@ -8,9 +8,9 @@ using namespace libstp::test;
 class DifferentialKinematicsTest : public KinematicsTestFixture {};
 
 TEST_F(DifferentialKinematicsTest, GetWheelRadiusReturnsConstructorValue) {
+    constexpr double wheelbase = 0.2;
+    constexpr double wheelRadius = 0.05;
     // Arrange
-    constexpr double wheelbase = 0.2;  // 200mm
-    constexpr double wheelRadius = 0.05;  // 50mm
 
     libstp::kinematics::differential::DifferentialKinematics kinematics(
         motors_[0].get(),
@@ -19,10 +19,8 @@ TEST_F(DifferentialKinematicsTest, GetWheelRadiusReturnsConstructorValue) {
         wheelRadius
     );
 
-    // Act
     double result = kinematics.getWheelRadius();
 
-    // Assert
     EXPECT_DOUBLE_EQ(result, wheelRadius);
 }
 
@@ -46,7 +44,6 @@ TEST_F(DifferentialKinematicsTest, GetWheelRadiusWithDifferentValue) {
 }
 
 TEST_F(DifferentialKinematicsTest, WheelCountIsTwo) {
-    // Arrange
     libstp::kinematics::differential::DifferentialKinematics kinematics(
         motors_[0].get(),
         motors_[1].get(),
@@ -54,12 +51,10 @@ TEST_F(DifferentialKinematicsTest, WheelCountIsTwo) {
         0.05  // wheelRadius
     );
 
-    // Act & Assert
     EXPECT_EQ(kinematics.wheelCount(), 2);
 }
 
 TEST_F(DifferentialKinematicsTest, SupportsLateralMotionIsFalse) {
-    // Arrange
     libstp::kinematics::differential::DifferentialKinematics kinematics(
         motors_[0].get(),
         motors_[1].get(),
@@ -67,6 +62,5 @@ TEST_F(DifferentialKinematicsTest, SupportsLateralMotionIsFalse) {
         0.05
     );
 
-    // Act & Assert
     EXPECT_FALSE(kinematics.supportsLateralMotion());
 }
