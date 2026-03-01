28be2effe14cf67e6e25b5c26a91e830141c2998
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-03-01 17:15:12 +0100

    feat: add version handling for libstp and raccoon-transport
    - Expose `__version__` for libstp via Pybind11 bindings
    - Print libstp and raccoon-transport version information during initialization
    - Extend build script to patch and restore versions for raccoon-transport
    - Refactor `patch_version` into `patch_version_file` for flexibility

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 
 namespace py = pybind11;
 
+// Stringify macro for VERSION_INFO
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
 PYBIND11_MODULE(_core, m) {
     m.doc() = "Python bindings for libstp";
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
 }
